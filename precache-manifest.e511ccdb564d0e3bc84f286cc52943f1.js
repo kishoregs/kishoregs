f66af08cf0c6164c6d04705578d5d4d053d24cb5
Kishore <kishor4u@gmail.com>
2020-10-17 21:10:31 -0400

    UI improvements2

--- precache-manifest.e511ccdb564d0e3bc84f286cc52943f1.js
+++ precache-manifest.e511ccdb564d0e3bc84f286cc52943f1.js
@@ -1,54 +1,54 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e932645c2036b79e11bf17da76a90e51",
-    "url": "/portfolio-website/index.html"
+    revision: "e932645c2036b79e11bf17da76a90e51",
+    url: "/kishoregs/index.html",
   },
   {
-    "revision": "010e5c4ae86cb5e753a4",
-    "url": "/portfolio-website/static/css/main.3a65ec82.chunk.css"
+    revision: "010e5c4ae86cb5e753a4",
+    url: "/kishoregs/static/css/main.3a65ec82.chunk.css",
   },
   {
-    "revision": "965ac76a91c0210cd85b",
-    "url": "/portfolio-website/static/js/2.eb989825.chunk.js"
+    revision: "965ac76a91c0210cd85b",
+    url: "/kishoregs/static/js/2.eb989825.chunk.js",
   },
   {
-    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/portfolio-website/static/js/2.eb989825.chunk.js.LICENSE.txt"
+    revision: "89b0379e7bcda1a468d8b0343aeb4e53",
+    url: "/kishoregs/static/js/2.eb989825.chunk.js.LICENSE.txt",
   },
   {
-    "revision": "010e5c4ae86cb5e753a4",
-    "url": "/portfolio-website/static/js/main.6c69bb10.chunk.js"
+    revision: "010e5c4ae86cb5e753a4",
+    url: "/kishoregs/static/js/main.6c69bb10.chunk.js",
   },
   {
-    "revision": "99cb685b86efd0f580da",
-    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+    revision: "99cb685b86efd0f580da",
+    url: "/kishoregs/static/js/runtime-main.c16362e1.js",
   },
   {
-    "revision": "94a502b4fa8f7325dfa9e70ebfd70f43",
-    "url": "/portfolio-website/static/media/CMIC.94a502b4.JPG"
+    revision: "94a502b4fa8f7325dfa9e70ebfd70f43",
+    url: "/kishoregs/static/media/CMIC.94a502b4.JPG",
   },
   {
-    "revision": "20568a6d17e8237437fa9dcb65d85de7",
-    "url": "/portfolio-website/static/media/PortfolioWebsiteScreenshot.20568a6d.JPG"
+    revision: "20568a6d17e8237437fa9dcb65d85de7",
+    url: "/kishoregs/static/media/PortfolioWebsiteScreenshot.20568a6d.JPG",
   },
   {
-    "revision": "497599d7f8638039a39c760281dd8515",
-    "url": "/portfolio-website/static/media/ToDoListScreenshot.497599d7.jpg"
+    revision: "497599d7f8638039a39c760281dd8515",
+    url: "/kishoregs/static/media/ToDoListScreenshot.497599d7.jpg",
   },
   {
-    "revision": "7147fc0c7ed71c3bdb55466c2479dce7",
-    "url": "/portfolio-website/static/media/background.7147fc0c.jpg"
+    revision: "7147fc0c7ed71c3bdb55466c2479dce7",
+    url: "/kishoregs/static/media/background.7147fc0c.jpg",
   },
   {
-    "revision": "25d5f08daf1104a18a53142b59b37173",
-    "url": "/portfolio-website/static/media/background2.25d5f08d.jpg"
+    revision: "25d5f08daf1104a18a53142b59b37173",
+    url: "/kishoregs/static/media/background2.25d5f08d.jpg",
   },
   {
-    "revision": "949a49b48e58e70119574e41de7b1ff4",
-    "url": "/portfolio-website/static/media/cssart.949a49b4.jpg"
+    revision: "949a49b48e58e70119574e41de7b1ff4",
+    url: "/kishoregs/static/media/cssart.949a49b4.jpg",
   },
   {
-    "revision": "dd217b77c606f682500827090fa655d5",
-    "url": "/portfolio-website/static/media/picture.dd217b77.jpg"
-  }
+    revision: "dd217b77c606f682500827090fa655d5",
+    url: "/kishoregs/static/media/picture.dd217b77.jpg",
+  },
 ]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/portfolio-website/precache-manifest.e511ccdb564d0e3bc84f286cc52943f1.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+importScripts(
+  "/kishoregs/precache-manifest.e511ccdb564d0e3bc84f286cc52943f1.js"
+);
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
@@ -33,7 +35,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/kishoregs/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
+  }
+);
