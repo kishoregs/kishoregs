ab7a71bf19da4728dd1d326ba3f342d2dbaa8389
Kishore <kishor4u@gmail.com>
2020-10-18 21:58:12 -0400

    UI improvements4

---
+++ precache-manifest.7b0a7d7a8395aaa4c3c37d564f25a66b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bcf862035b4ecebb64fe5d0ee51b0a",
+    "url": "/kishoregs/index.html"
+  },
+  {
+    "revision": "39ec4ea4e2b7b5888c66",
+    "url": "/kishoregs/static/css/main.3a65ec82.chunk.css"
+  },
+  {
+    "revision": "9659ac142b4a2fa5cbdd",
+    "url": "/kishoregs/static/js/2.04ee3eea.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/kishoregs/static/js/2.04ee3eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ec4ea4e2b7b5888c66",
+    "url": "/kishoregs/static/js/main.cc9f4aa0.chunk.js"
+  },
+  {
+    "revision": "aadc7f46f3b76953726c",
+    "url": "/kishoregs/static/js/runtime-main.422e15e5.js"
+  },
+  {
+    "revision": "94a502b4fa8f7325dfa9e70ebfd70f43",
+    "url": "/kishoregs/static/media/CMIC.94a502b4.JPG"
+  },
+  {
+    "revision": "20568a6d17e8237437fa9dcb65d85de7",
+    "url": "/kishoregs/static/media/PortfolioWebsiteScreenshot.20568a6d.JPG"
+  },
+  {
+    "revision": "497599d7f8638039a39c760281dd8515",
+    "url": "/kishoregs/static/media/ToDoListScreenshot.497599d7.jpg"
+  },
+  {
+    "revision": "7147fc0c7ed71c3bdb55466c2479dce7",
+    "url": "/kishoregs/static/media/background.7147fc0c.jpg"
+  },
+  {
+    "revision": "25d5f08daf1104a18a53142b59b37173",
+    "url": "/kishoregs/static/media/background2.25d5f08d.jpg"
+  },
+  {
+    "revision": "949a49b48e58e70119574e41de7b1ff4",
+    "url": "/kishoregs/static/media/cssart.949a49b4.jpg"
+  },
+  {
+    "revision": "dd217b77c606f682500827090fa655d5",
+    "url": "/kishoregs/static/media/picture.dd217b77.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kishoregs/precache-manifest.f9e01c17866d66b5a43ce098cf508e6b.js"
+  "/kishoregs/precache-manifest.7b0a7d7a8395aaa4c3c37d564f25a66b.js"
 );
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/kishoregs/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kishoregs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
