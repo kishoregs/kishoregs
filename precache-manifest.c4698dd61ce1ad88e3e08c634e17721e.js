b3c2e228d038b934e15c767b7ef39e4d573dc5d5
Kishore Shiraguppi <kishor4u@gmail.com>
2024-03-14 20:58:17 -0400

    updated

---
+++ precache-manifest.c4698dd61ce1ad88e3e08c634e17721e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ede09b01ba14ed0e029f9f840fd7c4",
+    "url": "/kishoregs/index.html"
+  },
+  {
+    "revision": "81df9311f31b6cc94044",
+    "url": "/kishoregs/static/css/main.239fc7fd.chunk.css"
+  },
+  {
+    "revision": "7df60df404aefab134a6",
+    "url": "/kishoregs/static/js/2.62157620.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/kishoregs/static/js/2.62157620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81df9311f31b6cc94044",
+    "url": "/kishoregs/static/js/main.c3ef8344.chunk.js"
+  },
+  {
+    "revision": "57c910500c8f0bf070aa",
+    "url": "/kishoregs/static/js/runtime-main.f67028ca.js"
+  },
+  {
+    "revision": "94a502b4fa8f7325dfa9e70ebfd70f43",
+    "url": "/kishoregs/static/media/CMIC.94a502b4.JPG"
+  },
+  {
+    "revision": "20568a6d17e8237437fa9dcb65d85de7",
+    "url": "/kishoregs/static/media/PortfolioWebsiteScreenshot.20568a6d.JPG"
+  },
+  {
+    "revision": "497599d7f8638039a39c760281dd8515",
+    "url": "/kishoregs/static/media/ToDoListScreenshot.497599d7.jpg"
+  },
+  {
+    "revision": "7147fc0c7ed71c3bdb55466c2479dce7",
+    "url": "/kishoregs/static/media/background.7147fc0c.jpg"
+  },
+  {
+    "revision": "25d5f08daf1104a18a53142b59b37173",
+    "url": "/kishoregs/static/media/background2.25d5f08d.jpg"
+  },
+  {
+    "revision": "949a49b48e58e70119574e41de7b1ff4",
+    "url": "/kishoregs/static/media/cssart.949a49b4.jpg"
+  },
+  {
+    "revision": "a014a6f0c3199179dd3ecd05a5775d54",
+    "url": "/kishoregs/static/media/kishore-pro.a014a6f0.png"
+  },
+  {
+    "revision": "892bd8f3c5b17b7bfe47e0b97f77151f",
+    "url": "/kishoregs/static/media/kvk-fp.892bd8f3.png"
+  },
+  {
+    "revision": "9ee50ca696866a5e285aab7c39956e1d",
+    "url": "/kishoregs/static/media/ml-fp.9ee50ca6.png"
+  },
+  {
+    "revision": "e710e4ef6190d7388e8b1d8c0831f497",
+    "url": "/kishoregs/static/media/qh-fp.e710e4ef.png"
+  },
+  {
+    "revision": "ea4a0fb03fa2942f08c97cbaa5a71c74",
+    "url": "/kishoregs/static/media/sd-fp.ea4a0fb0.png"
+  }
+]);
